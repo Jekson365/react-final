de22c0b37823ecbd49090b568492dba564056bb6
Jeko <jeko.erg@gamil.com>
2025-02-06 17:21:26 +0400

    cc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite '
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite config
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
